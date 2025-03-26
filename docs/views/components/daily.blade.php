01204be97f26ffe96b04a6881599baf191c84393
Zavorotna <zavorotnaa96@gmail.com>
2025-03-27 01:51:24 +0200

    create project

---
+++ docs/views/components/daily.blade.php
@@ -0,0 +1,1 @@
+<div class="table_daily"><h2>Daily Summary</h2><table><thead><tr><th>date</th><th>trades</th><th>lots</th><th>result</th></tr></thead><tbody><tr><td>3/13/2025</td><td>4</td><td>2.58</td><td class="positive">+$51.6</td></tr><tr><td>3/13/2025</td><td>4</td><td>2.58</td><td class="negative">-$51.6</td></tr></tbody></table></div>
