4f7cd29a174e05fb8b897c958d571fa3591fdd42
Zavorotna <zavorotnaa96@gmail.com>
2025-04-24 20:46:17 +0300

    change card

---
+++ docs/views/components/history_transaction.blade.php
@@ -0,0 +1,53 @@
+
+<div class="table_history_container">
+  <div class="table_history">
+    <table>
+      <thead> 
+        <tr> 
+          <th>id</th>
+          <th>amount</th>
+          <th>status</th>
+          <th>payment date</th>
+        </tr>
+      </thead>
+      <tbody> 
+        <tr>
+          <td>Lorem, ipsum dolor.</td>
+          <td>Iste, ab doloribus?</td>
+          <td>Labore, ipsa voluptates!</td>
+          <td>Quis, praesentium illo?</td>
+        </tr>
+        <tr>
+          <td>Lorem, ipsum dolor.</td>
+          <td>Fuga, dignissimos odit!</td>
+          <td>Doloribus, consequatur eveniet!</td>
+          <td>Vel, optio atque.</td>
+        </tr>
+        <tr>
+          <td>Lorem, ipsum dolor.</td>
+          <td>Veritatis, cupiditate velit.</td>
+          <td>Dolores, repellendus id.</td>
+          <td>Quis, minus nemo!</td>
+        </tr>
+        <tr>
+          <td>Lorem, ipsum dolor.</td>
+          <td>Voluptate, magni fuga.</td>
+          <td>Quia, architecto omnis.</td>
+          <td>Autem, aperiam maiores.</td>
+        </tr>
+        <tr>
+          <td>Lorem, ipsum dolor.</td>
+          <td>Excepturi, totam fugit?</td>
+          <td>Beatae, aperiam voluptatem!</td>
+          <td>Quis, esse modi!</td>
+        </tr>
+        <tr>
+          <td>Lorem, ipsum dolor.</td>
+          <td>Enim, placeat earum.</td>
+          <td>Molestiae, autem inventore?</td>
+          <td>In, magnam aut.</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
---
+++ docs/views/components/navigation_transaction.blade.php
@@ -0,0 +1,7 @@
+
+<nav class="transaction_nav">
+  <ul class="flex items-center gap">
+    <li> <a href="#"> Create transaction</a></li>
+    <li> <a class="active_btn" href="#"> transaction history</a></li>
+  </ul>
+</nav>
