a473344f5c14fa2a5bbaa0f1b8d3b307088939dc
Zavorotna <zavorotnaa96@gmail.com>
2025-04-24 18:40:15 +0300

    change card

--- docs/views/components/card.blade.php
+++ docs/views/components/card.blade.php
@@ -7,9 +7,6 @@
       <h2 class="price">5k$</h2>
       <p class="sub-price">36$</p>
     </div>
-    <form class="tac" action="#">
-      <button class="black_cta" type="submit">buy </button>
-    </form>
   </div>
 </div>
 <div class="card tac"><a class="mask" href="#"> </a>
@@ -20,9 +17,6 @@
       <h2 class="price">10k$</h2>
       <p class="sub-price">70$</p>
     </div>
-    <form class="tac" action="#">
-      <button class="black_cta" type="submit">buy </button>
-    </form>
   </div>
 </div>
 <div class="card tac"><a class="mask" href="#"> </a>
@@ -31,11 +25,8 @@
       <p class="step">2-step</p>
       <p class="evaluation">evaluation</p>
       <h2 class="price">25k$</h2>
-      <p class="sub-price">160$</p>
+      <p class="sub-price">160$ </p>
     </div>
-    <form class="tac" action="#">
-      <button class="black_cta" type="submit">buy </button>
-    </form>
   </div>
 </div>
 <div class="card tac"><a class="mask" href="#"> </a>
@@ -46,9 +37,6 @@
       <h2 class="price">50k$</h2>
       <p class="sub-price">270$</p>
     </div>
-    <form class="tac" action="#">
-      <button class="black_cta" type="submit">buy </button>
-    </form>
   </div>
 </div>
 <div class="card tac"><a class="mask" href="#"> </a>
@@ -59,8 +47,5 @@
       <h2 class="price">100k$</h2>
       <p class="sub-price">525$</p>
     </div>
-    <form class="tac" action="#">
-      <button class="black_cta" type="submit">buy </button>
-    </form>
   </div>
 </div>
